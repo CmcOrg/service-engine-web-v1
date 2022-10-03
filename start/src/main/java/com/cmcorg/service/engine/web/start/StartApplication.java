aa098762bec72c39597df62b0499bbe790f700b7
法外狂徒李四 <dimensional_logic@qq.com>
2022-10-03 12:43:29 +0800

    【i】【service-engine-web-v1】

---
+++ start/src/main/java/com/cmcorg/service/engine/web/start/StartApplication.java
@@ -0,0 +1,15 @@
+package com.cmcorg.service.engine.web.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
