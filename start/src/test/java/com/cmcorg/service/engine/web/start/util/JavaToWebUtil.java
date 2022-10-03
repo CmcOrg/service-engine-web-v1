2a33684d83f40466d168e6b3f5566b67adc69b08
法外狂徒李四 <dimensional_logic@qq.com>
2022-10-03 15:28:40 +0800

    【i】【service-engine-web-v1】

---
+++ start/src/test/java/com/cmcorg/service/engine/web/start/util/JavaToWebUtil.java
@@ -0,0 +1,11 @@
+package com.cmcorg.service.engine.web.start.util;
+
+import com.cmcorg.generate.page.h5.util.WebJsonToWebUtil;
+
+public class JavaToWebUtil {
+
+    public static void main(String[] args) {
+        WebJsonToWebUtil.exec(); // 生成前端页面
+    }
+
+}
