10cf094f6459bced0308e17096c3f1a43504b832
caoguiming <1677664380@qq.com>
2022-10-10 18:59:02 +0800

    【i】【CmcOrg】

--- web-start/src/test/java/com/cmcorg/service/engine/web/start/util/WebJavaToWebUtil.java
+++ web-start/src/test/java/com/cmcorg/service/engine/web/start/util/WebJavaToWebUtil.java
@@ -2,7 +2,7 @@ package com.cmcorg.service.engine.web.start.util;
 
 import com.cmcorg.generate.page.h5.util.WebJsonToWebUtil;
 
-public class JavaToWebUtil {
+public class WebJavaToWebUtil {
 
     public static void main(String[] args) {
         WebJsonToWebUtil.exec("D:/GitHub/CmcOrg/frontend-h5-web-v1/"); // 生成前端页面
