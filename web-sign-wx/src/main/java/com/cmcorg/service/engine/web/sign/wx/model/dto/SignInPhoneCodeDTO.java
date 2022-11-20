818275b9252ad9a80a31893b2cfd6f3af99bdb4c
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-20 14:32:58 +0800

    【i】【CmcOrg】

--- web-sign-wx/src/main/java/com/cmcorg/service/engine/web/sign/wx/model/dto/SignInPhoneCodeDTO.java
+++ web-sign-wx/src/main/java/com/cmcorg/service/engine/web/sign/wx/model/dto/SignInPhoneCodeDTO.java
@@ -1,5 +1,6 @@
 package com.cmcorg.service.engine.web.sign.wx.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 public class SignInPhoneCodeDTO {
 
     @NotBlank
+    @Schema(description = "手机号码 code")
     private String phoneCode;
 
 }
